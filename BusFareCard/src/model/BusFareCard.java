8ad131faed34091a4d242a78cf049a84c56951a9
Mary Songal <mary.a.songal@gmail.com>
2017-10-22 00:00:16 +0200

    BasFareCard tasks done

--- BusFareCard/src/model/BusFareCard.java
+++ BusFareCard/src/model/BusFareCard.java
@@ -32,36 +32,50 @@ public class BusFareCard {
     public double getBalance() { return balance; }
     public boolean isFareLoaded() { return fareLoaded; }
 
-    // REQUIRES: the age of this card must be > AGE_CUTOFF
-    //           the balance on this card must be >= 0 after purchasing the card
     // MODIFIES: this
     // EFFECTS: if the REQUIRES clause is fulfilled, subtract cost of the adult fare from the balance
     //          and set the isFareLoaded field to true
     public void purchaseAdultFare() throws IllegalAgeException, NoBalanceException {
-        //TODO: complete the implementation of this method
+        if (getAge() <= AGE_CUTOFF) {
+            throw new IllegalAgeException("The owner's age is invalid");
+        }
+        if (getBalance() < ADULT_FARE) {
+            throw new NoBalanceException("Not enough funds to purchase an adult ticket");
+        }
+        balance -= ADULT_FARE;
+        fareLoaded = true;
     }
 
-    // REQUIRES: the age of this card must be <= AGE_CUTOFF
-    //           the balance on this card must be >= 0 after purchasing the card
     // MODIFIES: this
     // EFFECTS: if the REQUIRES clauses is fulfilled, subtract cost of a concession fare from the balance
     //          and set the isFareLoaded field to true
     public void purchaseConcessionTicket() throws IllegalAgeException, NoBalanceException {
-        //TODO: complete the implementation of this method
+        if (getAge() > AGE_CUTOFF) {
+            throw new IllegalAgeException("The owner's age is invalid");
+        }
+        if (getBalance() < CONCESSION_FARE) {
+            throw new NoBalanceException("Not enough funds to purchase a concession ticket");
+        }
+        balance -= CONCESSION_FARE;
+        fareLoaded = true;
     }
 
-    // REQUIRES: amount you want to load onto the card must be > 0
     // MODIFIES: this
     // EFFECTS: loads the specified amount onto the card's balance field
     public void reloadBalance(double amount) throws IllegalAmountException {
-        //TODO: complete the implementation of this method
+        if (amount <= 0) {
+            throw new IllegalAmountException("The loaded amount should be more than zero");
+        }
+        balance = amount;
     }
 
-    // REQUIRES: the fareLoaded field must be true in order to "board" a bus
     // MODIFIES: this
     // EFFECTS: sets fareLoaded field to false, else throws exception
     public void boardBus() throws MissingFareException {
-        //TODO: complete the implementation of this method
+        if (!fareLoaded) {
+            throw new MissingFareException("No fare! You shall not pass!");
+        }
+        fareLoaded = false;
     }
 
 
